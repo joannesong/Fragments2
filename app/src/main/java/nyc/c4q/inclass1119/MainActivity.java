0b6f521a2d2881ec10d9199a75e1038369378c9c
Joanne Song <joannesong@ac.c4q.nyc>
2017-11-19 16:55:08 -0500

    first fragment

--- app/src/main/java/nyc/c4q/inclass1119/MainActivity.java
+++ app/src/main/java/nyc/c4q/inclass1119/MainActivity.java
@@ -1,7 +1,11 @@
 package nyc.c4q.inclass1119;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import nyc.c4q.inclass1119.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainFragment mainFragment = new MainFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment, mainFragment);
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/nyc/c4q/inclass1119/fragment/MainFragment.java
@@ -0,0 +1,63 @@
+package nyc.c4q.inclass1119.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import nyc.c4q.inclass1119.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    private View rootView;
+    EditText editText;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        Button button01 = (Button) rootView.findViewById(R.id.button1);
+        Button button02 = (Button) rootView.findViewById(R.id.button2);
+        Button button03 = (Button) rootView.findViewById(R.id.button3);
+        Button button04 = (Button) rootView.findViewById(R.id.button4);
+        Button button05 = (Button) rootView.findViewById(R.id.button5);
+
+        editText = (EditText) rootView.findViewById(R.id.main_fragment_edittext)
+
+
+        button01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment01 fragment01 = new Fragment01();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                String textFromEditText = editText.getText().toString();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("text", textFromEditText);
+                fragment01.setArguments(bundle);
+
+                fragmentTransaction.replace(R.id.main_fragment, fragment01).addToBackStack("fragment01");
+                fragmentTransaction.commit();
+
+            }
+        });
+        return rootView;
+    }
+
+}
